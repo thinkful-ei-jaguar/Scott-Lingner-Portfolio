995c88f46d6d44b29e36430f432b8e3c1bc76cc5
Scott Lingner <slingner@gmail.com>
2019-12-20 15:36:45 -0800

    add hamburger, add project in main page, css styling

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-$('.splash-nav-toggle').click(event => {
-  const sibling = $(event.target).siblings('.splash-nav-dropdown');
-  const isHidden = sibling.hasClass('splash-nav-dropdown-hidden');
-  // Hide all dropdowns
-  $('.splash-nav-dropdown').addClass('splash-nav-dropdown-hidden');
-
-  if (isHidden) {
-    // Show the one we selected if it was hidden before
-    sibling.removeClass('splash-nav-dropdown-hidden');
-  }
+$('.hamburger').click(function(){
+  $('.menu').toggleClass('hidden');
 });
-$('.hamburger').click(event => {
-  $('.splash-nav-desktop-grid').toggleClass('splash-nav-desktop-grid-closed');
+
+$('.close').click(function(){
+  $('.menu').toggleClass('hidden');
+});
+
+$('.links').click(function() {
+  $('.menu').toggleClass('hidden');
 });
