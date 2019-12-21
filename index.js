9ca79bd4735c4780d95b933581d7274035a6d532
Scott Lingner <slingner@gmail.com>
2019-12-20 20:33:01 -0800

    add animate for scroll

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ $('.links').click(function() {
   $('.menu').toggleClass('hidden');
 });
 
+$('.workScroll').click(function() {
+  $('html,body').animate({
+    scrollTop: $('#Work').offset().top},
+  'slow');
+});
+
+$('.contactScroll').click(function() {
+  $('html,body').animate({
+    scrollTop: $('#contact').offset().top},
+  'slow');
+});
